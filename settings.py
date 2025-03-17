419c01342dffd4fce8fe9a879314b247a949a186
davidAlgis <david.algis@tutamail.com>
2025-03-17 11:01:04 +0100

    Refactor and remove useless print

--- settings.py
+++ settings.py
@@ -6,14 +6,15 @@ class languageToolSettings:
     def __init__(self, parent=None):
         self.parent = parent
         self.global_settings = sublime.load_settings(
-            "LanguageTool.sublime-settings")
+            "LanguageTool.sublime-settings"
+        )
 
     def get(self, key, default=None):
         window = sublime.active_window()
         view = window.active_view()
-        project_languageTool_settings = view.settings().get(
-            "LanguageTool", {}) or {}
-        print(project_languageTool_settings.keys)
+        project_languageTool_settings = (
+            view.settings().get("LanguageTool", {}) or {}
+        )
         if key in project_languageTool_settings:
             return project_languageTool_settings[key]
 
